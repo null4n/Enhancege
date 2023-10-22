28acd0fe1e6577a28de5c6353fc998759fdf7a37
null4n <null4n@github.com>
2023-10-22 11:18:54 +0800

    Init

---
+++ app/src/main/java/com/example/enhancege/MainHook.kt
@@ -0,0 +1,48 @@
+package com.example.enhancege
+
+import android.content.Context
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodReplacement
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class MainHook : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        if (lpparam.packageName == "com.sonymobile.gameenhancer") {
+            //OneHandedMode detect
+            XposedHelpers.findAndHookMethod(
+                "com.sonymobile.gameenhancer.floating.mode.AccessibilitySettingHelper",
+                lpparam.classLoader, "isOneHandedModeServiceDisabled",
+                object : XC_MethodReplacement() {
+                    override fun replaceHookedMethod(param: MethodHookParam?): Any {
+                        return true
+                    }
+                }
+            )
+
+            //Accessibility conflict detect
+            XposedHelpers.findAndHookMethod(
+                "com.sonymobile.gameenhancer.common.util.DeviceStatusUtil",
+                lpparam.classLoader, "isConflicted",
+                Context::class.java, List::class.java,
+                object : XC_MethodReplacement() {
+                    override fun replaceHookedMethod(param: MethodHookParam?): Any {
+                        return false
+                    }
+                }
+            )
+
+            //Blacklist package detect
+            XposedHelpers.findAndHookMethod(
+                "com.sonymobile.gameenhancer.database.roomdb.GameDatabase",
+                lpparam.classLoader, "isBlacklisted",
+                String::class.java,
+                object : XC_MethodReplacement() {
+                    override fun replaceHookedMethod(param: MethodHookParam?): Any {
+                        return false
+                    }
+                }
+            )
+        }
+    }
+}
